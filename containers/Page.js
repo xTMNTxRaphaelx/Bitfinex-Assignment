7fcc87e49f4b4afe89c47ab353d2d8a19ec35ef9
Rahul Garg <rahul_garg08@yahoo.com>
2019-10-01 16:59:40 +0530

    work done earlier

--- containers/Page.js
+++ containers/Page.js
@@ -18,7 +18,7 @@ function Page(props) {
     props.initWS();
   }, []);
   return (
-    <View style={{ flex: 1 }}>
+    <View>
       <View
         style={{
           flexDirection: 'row',
@@ -43,53 +43,12 @@ function Page(props) {
         >
           {props.symbol}
         </Text>
-        <Button
-          onPress={e => setModal(true)}
-          style={{ padding: 9, marginLeft: 9 }}
-          color="#f194ff"
-          title="Change"
-        />
       </View>
       <ScrollView style={{ flex: 1, backgroundColor: '#2b3940' }}>
         <Ticker tickerData={props.tickerData} symbol={props.symbol} />
         <TradeTable tradeData={props.tradeData} />
         <OrderTable orderData={props.orderData} />
       </ScrollView>
-      <Modal
-        animationType={'slide'}
-        transparent={false}
-        visible={showCurrencyModal}
-        onRequestClose={() => {
-          console.log('Modal has been closed.');
-        }}
-      >
-        <View>
-          <Button
-            style={{ padding: 9, marginBottom: 9 }}
-            color="#f194ff"
-            onPress={e => props.setSymbol('tBTCUSD')}
-            title="BTC/USD"
-          />
-          <Button
-            style={{ padding: 9, marginBottom: 9 }}
-            color="#f194ff"
-            onPress={e => props.setSymbol('tLTCUSD')}
-            title="LTC/USD"
-          />
-          <Button
-            style={{ padding: 9, marginBottom: 9 }}
-            color="#f194ff"
-            onPress={e => props.setSymbol('tETHUSD')}
-            title="ETH/USD"
-          />
-          <Button
-            title="Click To Close Modal"
-            onPress={() => {
-              setModal(!showCurrencyModal);
-            }}
-          />
-        </View>
-      </Modal>
     </View>
   );
 }
