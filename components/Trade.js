1b5afe580c599b0347db25e60339eb695ad970a1
Rahul Garg <rahul_garg08@yahoo.com>
2019-10-01 11:31:51 +0530

    collapsible trade view

--- components/Trade.js
+++ components/Trade.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
+import {
+  Collapse,
+  CollapseHeader,
+  CollapseBody,
+  AccordionList
+} from 'accordion-collapse-react-native';
+import { Table, Row, Rows } from 'react-native-table-component';
 
 function Trade({ tradeData = [] }) {
   console.log('is trading', tradeData);
+  const tableHead = ['TIME', 'AMOUNT', 'PRICE'];
+  const tableData = tradeData.map(data => {
+    const [id, ...rest] = data;
+    return rest;
+  });
   return (
-    <View>
-      <Text>Trade</Text>
-      {tradeData.map(data => {
-        return <Text key={data[0]}>{data}</Text>;
-      })}
-    </View>
+    <Collapse>
+      <CollapseHeader>
+        <View>
+          <Text>Trade</Text>
+        </View>
+      </CollapseHeader>
+      <CollapseBody>
+        <ScrollView>
+          <Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff' }}>
+            <Row data={tableHead} />
+            <Rows data={tableData} />
+          </Table>
+        </ScrollView>
+      </CollapseBody>
+    </Collapse>
   );
 }
 
